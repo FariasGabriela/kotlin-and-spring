d301cdc16574fbcf5a14267382e5fb62fd11fc44
FariasGabriela <gabrielafarias@alunos.utfpr.edu.br>
2024-01-09 11:24:29 -0300

    Commit inicial - criação do projeto

---
+++ kotlinwithspring/src/test/kotlin/com/example/kotlinwithspring/KotlinwithspringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinwithspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinwithspringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
